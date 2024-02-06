2ad2dd29a25d442730fa01cfda34a08c78f1d6fd
LSierraOsorio <luis.asierraosorio99@gmail.com>
2024-02-06 11:05:35 -0800

    set up basic routing
    - routing for home page
    - routing for sign up sign in page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUpIn from "./pages/SignUpIn";
 
 function App() {
-
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home></Home>}></Route>
+          <Route path="/home" element={<Home></Home>}></Route>
+          <Route path="/register" element={<SignUpIn></SignUpIn>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
